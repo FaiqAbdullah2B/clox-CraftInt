d4172b75b109501c533b3feac0e8ce8deaa6539c
Faiq Abdullah <faiqabdullah2b@gmail.com>
2026-04-28 20:02:00 +0500

    did everything upto 26.4

--- memory.c
+++ memory.c
@@ -1,20 +1,51 @@
 #include <stdlib.h>
 
+#include "compiler.h"
 #include "memory.h"
 #include "vm.h"
 
-void* reallocate(void *pointer, size_t oldSize, size_t newSize) {
-    if (newSize == 0) {
-        free(pointer);
-        return NULL;
-    }
+#ifdef DEBUG_LOG_GC
+#include <stdio.h>
+#include "debug.h"
+#endif
 
-    void *result = realloc(pointer, newSize);
-    if (result == NULL) exit(1);
-    return result;
+void* reallocate(void *pointer, size_t oldSize, size_t newSize) {
+  if (newSize > oldSize) {
+#ifdef DEBUG_STRESS_GC
+    collectGarbage();
+#endif
+  }
+
+  if (newSize == 0) {
+      free(pointer);
+      return NULL;
+  }
+
+  void *result = realloc(pointer, newSize);
+  if (result == NULL) exit(1);
+  return result;
+}
+
+void markObject(Obj* object) {
+  if (object == NULL) return;
+
+#ifdef DEBUG_LOG_GC
+  printf("%p mark ", (void*)object);
+  printValue(OBJ_VAL(object));
+  printf("\n");
+#endif
+
+  object->isMarked = true;
+}
+
+void markValue(Value value) {
+  if (IS_OBJ(value)) markObject(AS_OBJ(value));
 }
 
 static void freeObject(Obj* object) {
+#ifdef DEBUG_LOG_GC
+  printf("%p free type %d\n", (void*)object, object->type);
+#endif
   switch (object->type) {
     case OBJ_NATIVE:
       FREE(ObjNative, object);
@@ -44,7 +75,38 @@ static void freeObject(Obj* object) {
   }
 }
 
-void freeObjects() {
+static void markRoots(void) {
+  for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+    markValue(*slot);
+  }
+
+  for (int i = 0; i < vm.frameCount; i++) {
+    markObject((Obj*)vm.frames[i].closure);
+  }
+
+  for (ObjUpvalue* upvalue = vm.openUpvalues;
+       upvalue != NULL;
+       upvalue = upvalue->next) {
+    markObject((Obj*)upvalue);
+  }
+
+  markTable(&vm.globals);
+  markCompilerRoots();
+}
+
+void collectGarbage(void) {
+#ifdef DEBUG_LOG_GC
+  printf("-- gc begin\n");
+#endif
+
+  markRoots();
+
+#ifdef DEBUG_LOG_GC
+  printf("-- gc end\n");
+#endif
+}
+
+void freeObjects(void) {
   Obj* object = vm.objects;
   while (object != NULL) {
     Obj* next = object->next;
